4c17fec72f24057479092d9b96c7415eec7068dd
Jayklef <jerryarhawho@gmail.com>
2024-02-05 07:15:17 +0000

    Complete part

--- css/scripts/calculator.js
+++ css/scripts/calculator.js
@@ -1,18 +1,62 @@
+function equivalentCheck(){
+    if(parent(document.getElementById('equivalent').value)){
+
+        document.getElementById('equivalent').value = 0;
+        document.getElementById('result').value = 0;
+    }
+}
+
+
 function input(x){
-    if (document.getElementById('result').value == 0){
+
+    equivalentCheck();
+
+    let y = parseFloat(document.getElementById('result').value);
+
+    if (document.getElementById('decimaiVar').value = 0){
+
+        x += y * 10;
+
         document.getElementById('result').value = x;
     }
 
     else{
-        document.getElementById('result').value += x;
+        let decimalCount = document.getElementById('result').value;
+
+        if(decimalCount == 1){
+            x*= 1/10;
+            y += x;
+
+            document.getElementById('result').value = y;
+        }
+        else{
+            document.getElementById('result').value += x;
+        }
+
+        decimalCount++;
+        document.getElementById('decimalVar').value = decimalCount;
     }
 }
+
+
+function decimalPoint(){
+    if(document.getElementById('decimalVar').value == 0){
+
+        document.getElementById('decimalVar').value = 1;
+    }
+
+    if(parseInt(document.getElementById('operation').value)){
+        document.getElementById('result').value = 0;
+    }
+}
+
+
 
 function operandCheck(){
     if(document.getElementById('operand').value == ""){
         document.getElementById('operand').value = document.getElementById('result').value;
 
-        document.getElementById('result').value = 0;
+        document.getElementById('equivalent').value = 1;
     }
 
     else{
@@ -46,6 +90,7 @@ function operatorCheck(){
 
     document.getElementById('operand').value = a;
     document.getElementById('result').value = a;
+    document.getElementById('equivalent').value = 1;
 }
 
 function operators(x){
@@ -67,11 +112,28 @@ function operators(x){
 
         default:
     }
+
+    operatorCheck();
+}
+
+
+function equals(){
+    operators(parseInt(document.getElementById('operation').value));
+
+    document.getElementById('result').value = document.getElementById('operand').value;
+
+    document.getElementById('operand').value = "";
+
+    document.getElementById('equivalent').value = 1;
 }
 
 
 function allClear(){
     document.getElementById('result').value = 0;
+    document.getElementById('operand').value = "";
+    document.getElementById('operation').value = 0;
+
+    document.getElementById('equivalent').value = 0;
 }
 
 function plusminus(){
